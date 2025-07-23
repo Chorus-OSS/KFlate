5c0aa98a64cb8fe10c4d2ef1578fc4acd5de2a73
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-07-24 00:57:12 +1200

    rem stuff

--- src/commonMain/kotlin/org/chorus_oss/kflate/lz77/LZ77Token.kt
+++
@@ -1,6 +0,0 @@
-package org.chorus_oss.kflate.lz77
-
-internal sealed interface LZ77Token {
-    data class Literal(val value: Byte) : LZ77Token
-    data class Match(val offset: UShort, val length: UShort) : LZ77Token
-}
