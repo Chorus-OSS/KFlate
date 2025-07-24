08d0323c3e3c0e38c39791d290bccb7f0e5eb141
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-07-25 05:08:56 +1200

    methods for ByteString compression & decompression

--- src/commonMain/kotlin/org/chorus_oss/kflate/Compressor.kt
+++ src/commonMain/kotlin/org/chorus_oss/kflate/Compressor.kt
@@ -1,5 +1,9 @@
 package org.chorus_oss.kflate
 
+import kotlinx.io.bytestring.ByteString
+
 interface Compressor {
     fun compress(data: ByteArray): ByteArray
+
+    fun compress(data: ByteString): ByteString = ByteString(compress(data.toByteArray()))
 }
--- src/commonMain/kotlin/org/chorus_oss/kflate/Decompressor.kt
+++ src/commonMain/kotlin/org/chorus_oss/kflate/Decompressor.kt
@@ -1,5 +1,9 @@
 package org.chorus_oss.kflate
 
+import kotlinx.io.bytestring.ByteString
+
 interface Decompressor {
     fun decompress(data: ByteArray): ByteArray
+
+    fun decompress(data: ByteString): ByteString = ByteString(decompress(data.toByteArray()))
 }
