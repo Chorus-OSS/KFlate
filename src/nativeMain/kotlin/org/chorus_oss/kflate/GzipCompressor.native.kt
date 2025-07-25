1c6df3d5a065ea073c0b798803948299d3a35dfc
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-07-25 17:12:10 +1200

    impl native GzipCompressor * GzipDecompressor

--- src/nativeMain/kotlin/org/chorus_oss/kflate/GzipCompressor.native.kt
+++ src/nativeMain/kotlin/org/chorus_oss/kflate/GzipCompressor.native.kt
@@ -1,3 +1,67 @@
 package org.chorus_oss.kflate
 
-internal actual fun platformGzipCompressor(): Compressor = TODO("Not yet implemented")
+import kotlinx.cinterop.*
+import kotlinx.io.Buffer
+import kotlinx.io.IOException
+import kotlinx.io.readByteArray
+import platform.zlib.*
+
+@OptIn(ExperimentalForeignApi::class)
+internal class NativeGzipCompressor : Compressor {
+    override fun compress(data: ByteArray): ByteArray {
+        if (data.isEmpty()) return byteArrayOf()
+
+        val buf = Buffer()
+        memScoped {
+            val inBytes = data.toUByteArray()
+            val outBytes = UByteArray(16384)
+            val zStream = alloc<z_stream>()
+            zStream.zalloc = null
+            zStream.zfree = null
+            zStream.opaque = null
+            var code = deflateInit2(
+                zStream.ptr,
+                Z_DEFAULT_COMPRESSION,
+                Z_DEFLATED,
+                16 + 15,
+                8,
+                Z_DEFAULT_STRATEGY,
+            )
+
+            if (code != Z_OK) throw IOException("gzip compression failed, code: $code")
+
+            val inBytesPtr = inBytes.pin()
+            val outBytesPtr = outBytes.pin()
+            try {
+                zStream.next_in = inBytesPtr.addressOf(0)
+                zStream.avail_in = inBytes.size.toUInt()
+                zStream.next_out = outBytesPtr.addressOf(0)
+                zStream.avail_out = outBytes.size.toUInt()
+
+                var flush = Z_NO_FLUSH
+                do {
+                    if (zStream.avail_in.toInt() == 0) flush = Z_FINISH
+
+                    code = deflate(zStream.ptr, flush)
+
+                    if (code != Z_OK && code != Z_STREAM_END) throw IOException("gzip compression failed, code: $code")
+
+                    if (zStream.avail_out == 0u) {
+                        buf.write(outBytes.toByteArray())
+                        zStream.next_out = outBytesPtr.addressOf(0)
+                        zStream.avail_out = outBytes.size.toUInt()
+                    }
+                } while (code == Z_OK)
+                buf.write(outBytes.copyOfRange(0, outBytes.size - zStream.avail_out.toInt()).toByteArray())
+            } finally {
+                inBytesPtr.unpin()
+                outBytesPtr.unpin()
+                deflateEnd(zStream.ptr)
+            }
+        }
+
+        return buf.readByteArray()
+    }
+}
+
+internal actual fun platformGzipCompressor(): Compressor = NativeGzipCompressor()
--- src/nativeMain/kotlin/org/chorus_oss/kflate/GzipDecompressor.native.kt
+++ src/nativeMain/kotlin/org/chorus_oss/kflate/GzipDecompressor.native.kt
@@ -1,3 +1,59 @@
 package org.chorus_oss.kflate
 
-internal actual fun platformGzipDecompressor(): Decompressor = TODO("Not yet implemented")
+import kotlinx.cinterop.*
+import kotlinx.io.Buffer
+import kotlinx.io.IOException
+import kotlinx.io.readByteArray
+import platform.zlib.*
+
+@OptIn(ExperimentalForeignApi::class)
+internal class NativeGzipDecompressor : Decompressor {
+    override fun decompress(data: ByteArray): ByteArray {
+        if (data.isEmpty()) return byteArrayOf()
+
+        val buf = Buffer()
+        memScoped {
+            val inBytes = data.toUByteArray()
+            val outBytes = UByteArray(16384)
+            val zStream = alloc<z_stream>()
+            zStream.zalloc = null
+            zStream.zfree = null
+            zStream.opaque = null
+            var code = inflateInit2(
+                zStream.ptr,
+                16 + 15,
+            )
+
+            if (code != Z_OK) throw IOException("gzip decompression failed, code: $code")
+
+            val inBytesPtr = inBytes.pin()
+            val outBytesPtr = outBytes.pin()
+            try {
+                zStream.next_in = inBytesPtr.addressOf(0)
+                zStream.avail_in = inBytes.size.toUInt()
+                zStream.next_out = outBytesPtr.addressOf(0)
+                zStream.avail_out = outBytes.size.toUInt()
+                do {
+                    code = inflate(zStream.ptr, Z_NO_FLUSH)
+
+                    if (code != Z_OK && code != Z_STREAM_END) throw IOException("gzip decompression failed, code: $code")
+
+                    if (zStream.avail_out == 0u) {
+                        buf.write(outBytes.toByteArray())
+                        zStream.next_out = outBytesPtr.addressOf(0)
+                        zStream.avail_out = outBytes.size.toUInt()
+                    }
+                } while (code == Z_OK)
+                buf.write(outBytes.copyOfRange(0, outBytes.size - zStream.avail_out.toInt()).toByteArray())
+            } finally {
+                inBytesPtr.unpin()
+                outBytesPtr.unpin()
+                inflateEnd(zStream.ptr)
+            }
+        }
+
+        return buf.readByteArray()
+    }
+}
+
+internal actual fun platformGzipDecompressor(): Decompressor = NativeGzipDecompressor()
